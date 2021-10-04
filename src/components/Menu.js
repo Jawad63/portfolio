55b59f7e546d691ab583024ca4c20d73ed9fe75e
Jawad63 <jawadqureshi111@gmail.com>
2021-10-04 23:18:41 +0200

    navbar complete

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,36 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiMenu } from "react-icons/fi";
 
 
 const Menu = () => {
+   const [isOpen, setIsOpen] = useState(false);
    return (
       <>
          
          <div className="menu-btn">
-               <button>
-                  <FiMenu />
-               </button>
+            <button onClick={() => setIsOpen(!isOpen)}>
+               <FiMenu />
+            </button>
          </div>
 
-         <header className="header">
-
+         
+         <header className={`${isOpen ? "header open" : "header"}`}>
             <nav>
                <ul>
-                  <li>
+                  <li onClick={() => setIsOpen(false)}>
                      <Link to="/">Homepage</Link>
                   </li>
 
-                  <li>
+                  <li onClick={() => setIsOpen(false)}>
                      <Link to="/projects">Projects</Link>
                   </li>
 
-                  <li>
+                  <li onClick={() => setIsOpen(false)}>
                      <Link to="/contact-me">Contact Me</Link>
                   </li>
                </ul>
             </nav>
-
          </header>
+         
       </> 
    )
 }
