fc41ea152a2b074b4fe07c4cb25b2eb694602b0b
Jawad63 <jawadqureshi111@gmail.com>
2021-10-05 15:07:12 +0200

    creating projects page

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const projects = [
+   {
+      id: uuidv4(),
+      title: "Project One",
+      desc: "This is my first project",
+      github: "",
+      demo: "",
+      image: "./images/project-1.jpeg",
+   },
+
+   {
+      id: uuidv4(),
+      title: "Project Two",
+      desc: "This is my second project",
+      github: "",
+      demo: "",
+      image: "./images/project-2.jpeg",
+   },
+
+   {
+      id: uuidv4(),
+      title: "Project Three",
+      desc: "This is my third project",
+      github: "",
+      demo: "",
+      image: "./images/project-3.jpeg",
+   },
+]
