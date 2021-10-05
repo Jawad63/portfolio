c838ea4f002460c9d34c61a37b04953328072736
Jawad63 <jawadqureshi111@gmail.com>
2021-10-05 11:32:26 +0200

    contact info added.

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,9 +1,18 @@
-
-
 const Contacts = () => {
    return (
       <>
          <h1>Contact</h1>
+         <section className="contacts">
+            <ul>
+               <li>
+                  <a href="tel:+32465288957">+32 465 28 89 57</a>
+               </li>
+
+               <li>
+                  <a href="mailto:jawadqureshi111@gmail.com">jawadqureshi111@gmail.com</a>
+               </li>
+            </ul>
+         </section>
       </>
    )
 }
